b788ab0c3bc26d9db3f55a21ef9e0bb0509f5596
Morke-Ulv <morke651@hotmail.com>
2020-03-28 15:18:28 -0400

    first commit

---
+++ ConsoleAppGames/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program newGame = new Program();
+            newGame.Run();
+
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+            introQuote();
+        }
+        public void introQuote()
+        {
+            Console.Clear();
+            Console.WriteLine("In a galixy far far away....");
+            Console.WriteLine("Galaga hears rumor that Earth is under attack and must make haste to save the world");
+            Console.WriteLine("This is where your journey begins...");
+            Console.ReadKey(true);
+            room1();
+        }
+
+        public void room1()
+        {
+            Randy room = new Randy();
+            bool inRoom = true;
+            while (inRoom)
+            {
+            Console.Clear();
+            Console.WriteLine("Current position is in Galaxy 778, nearest points of jump is....");
+            Console.WriteLine("galaxy 334, galaxy 938, galaxy 492.");
+            Console.WriteLine("where would you like to jump to?");
+            string nextRoom = Console.ReadLine();
+            Console.Clear();
+                switch (nextRoom)
+                {
+                    case "galaxy 334":
+                        room.Randomizer();
+                        inRoom = false;
+                        break;
+                    case "galaxy 938":
+                        room.Randomizer();
+                        inRoom = false;
+                        break;
+                    case "galaxy 492":
+                        room.Randomizer();
+                        inRoom = false;
+                        break;
+                    default:
+                        Console.WriteLine("I did not copy that Captain");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppGames/Randy.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGames
+{
+    public class Randy
+    {
+        public void Randomizer()
+        {
+            Random rnd = new Random();
+            int randomRoom = rnd.Next(1, 11);
+            switch (randomRoom)
+            {
+                case 1:
+                    SurpriseAttack();
+                    break;
+                case 2:
+                    AstroidField();
+                    break;
+                case 3:
+                    Void();
+                    break;
+                case 4:
+                    FoundAnEnemy();
+                    break;
+                case 5:
+                    MedShip();
+                    break;
+                case 6:
+                    FuelBay();
+                    break;
+                case 7:
+                    Console.WriteLine("Energy Recharge?");
+                    break;
+                case 8:
+                    FoundAnEnemy();
+                    break;
+                case 9:
+                    SurpriseAttack();
+                    break;
+                case 10:
+                    FoundAnEnemy();
+                    break;
+            }
+        }
+
+        public void SurpriseAttack()
+        {
+            bool surpriseAttack = true;
+            while (surpriseAttack)
+            {
+            Console.Clear();
+            Console.WriteLine("CAPTAIN WE ARE UNDER ATTACK!!!!!!\n" +
+                "WHAT ARE YOUR ORDERS SIR?!?!?");
+            Console.WriteLine("Return Fire? Flee?");
+            string orders = Console.ReadLine();
+
+                switch (orders)
+                {
+                    case "Return Fire":
+                        Console.WriteLine("Yes sir!");
+                        surpriseAttack = false;
+                        break;
+                    case "Flee":
+                        Console.WriteLine("Getting us out of here Captain!");
+                        //random next room cost more fuel
+                        surpriseAttack = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("CAPTAIN PLEASE REPEAT!!!!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        public void AstroidField()
+        {
+            bool astroidField = true;
+            while (astroidField)
+            {
+                Console.Clear();
+                Console.WriteLine("Sir, we are enter an astroid field,\n" +
+                    "what are your orders?\n" +
+                    "Evasive Maneuvers? Retreat?");
+                string orders = Console.ReadLine();
+                switch (orders)
+                {
+                    case "Evasive Maneuvers":
+                        Console.WriteLine("Taking Evasive Manuevers");
+                        astroidField = false;
+                        break;
+                    case "Retreat":
+                        Console.WriteLine("Returning to previous galaxy");
+                        astroidField = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Could you please repeat Captain");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+
+        public void Void()
+        {
+            Console.Clear();
+            Console.WriteLine("There is nothing to report Captain");
+        }
+
+        public void FoundAnEnemy()
+        {
+            bool foundAnEnemy = true;
+            while (foundAnEnemy)
+            {
+                Console.Clear();
+                Console.WriteLine("Sir, Enemy on Radar.\n" +
+                    "what are your orders Captain?\n" +
+                    "Engage? Retreat? Ignore?");
+                string orders = Console.ReadLine();
+                switch (orders)
+                {
+                    case "Engage":
+                        Console.Clear();
+                        Console.WriteLine("Enaging enemy");
+                        foundAnEnemy = false;
+                        break;
+                    case "Retreat":
+                        Console.Clear();
+                        Console.WriteLine("Disengaging enemy");
+                        foundAnEnemy = false;
+                        break;
+                    case "Ignore":
+                        Console.Clear();
+                        Console.WriteLine("Ignoring the enemy");
+                        foundAnEnemy = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please repeat Sir.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        public void MedShip()
+        {
+            bool medShip = true;
+            while (medShip)
+            {
+                Console.Clear();
+                Console.WriteLine("Med ship up ahead Captain shall we stop?\n" +
+                    "Yes? No?");
+                string orders = Console.ReadLine();
+                switch (orders)
+                {
+                    case "Yes":
+                        Console.Clear();
+                        Console.WriteLine("Stoping for ship repairs this may take a little time Captain");
+                        medShip = false;
+                        break;
+                    case "No":
+                        Console.Clear();
+                        Console.WriteLine("Continuing on Sir.");
+                        medShip = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please repeat Sir");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        public void FuelBay()
+        {
+            bool fuelBay = true;
+            while (fuelBay)
+            {
+                Console.Clear();
+                Console.WriteLine("Sir, there is a fuel bay ahead shap we stop and refuel?\n" +
+                    "Yes? No?");
+                string orders = Console.ReadLine();
+                switch (orders)
+                {
+                    case "Yes":
+                        Console.Clear();
+                        Console.WriteLine("Refueling, this may take a little time Captain");
+                        fuelBay = false;
+                        break;
+                    case "No":
+                        Console.Clear();
+                        Console.WriteLine("Continuing on Sir.");
+                        fuelBay = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please repeat Sir");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        public void EnergyStation()
+        {
+
+        }
+    }
+}
